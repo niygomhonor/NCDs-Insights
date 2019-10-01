9c7f92aa06b323874741dfe890b7476d6f683516
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-10-01 15:05:43 +0200

    writing the content

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,23 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.post;
+
+
 public class App {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+
+        get("/about", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "about.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
 }
